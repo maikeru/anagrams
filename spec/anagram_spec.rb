4be41bd535088f8470c33409b725318a3abf095e
P. Michael Holland <maikeru.horando@gmail.com>
2013-11-23 12:53:54 +0900

    Change spec to expect syntax

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,6 +3,6 @@ require 'anagram'
 describe Anagram do
   let(:anagram) { Anagram.new }
   it "returns an empty list for an empty list" do
-    anagram.find(Array.new).should be_empty
+    expect(anagram.find(Array.new)).to be_empty
   end
 end
