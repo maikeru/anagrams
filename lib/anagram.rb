8b4af3a9be97a9ea1572a92030f5921e91dc2556
P. Michael Holland <maikeru.horando@gmail.com>
2013-11-23 14:15:56 +0900

    Shorten algorithm

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,12 +6,8 @@ class Anagram
       dictionary[key] ||= Array.new
       dictionary[key] << word
     end
-    result = Array.new
-    dictionary.each_value do |words|
-      if words.length > 1
-        result. << words
-      end
+    dictionary.values.select do |words|
+      words.length > 1
     end
-    return result
   end
 end
