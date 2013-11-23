361b5b6f67164916741114c09cbbeb06bf755d7c
P. Michael Holland <maikeru.horando@gmail.com>
2013-11-23 13:42:29 +0900

    Finds anagrams from list

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,15 @@
 class Anagram
   def find word_list
-    return Array.new
+    dictionary = Hash.new
+    word_list.each do |word|
+      key = word.downcase.chars.sort.join
+      dictionary[key] ||= Array.new
+      dictionary[key].push word
+    end
+    result = word_list.select do |word|
+      key = word.downcase.chars.sort.join
+      true if dictionary[key].length > 1
+    end
+    return result
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,4 +5,14 @@ describe Anagram do
   it "returns an empty list for an empty list" do
     expect(anagram.find(Array.new)).to be_empty
   end
+
+  it "returns all empty list when no anagrams exist" do
+    input = %w[a b c d]
+    expect(anagram.find(input)).to match_array Array.new
+  end
+
+  it "returns just the anagrams" do
+    input = %w[ruby rots sort file list]
+    expect(anagram.find(input)).to match_array %w[rots sort]
+  end
 end
